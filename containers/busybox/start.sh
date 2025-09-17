1fd7c17af447edd3c22181de08ad8db4ac026079
prathamshah94 <pratham.shah@cleanstart.com>
2025-09-17 16:58:35 +0530

    Delete containers/busybox/start.sh
    Signed-off-by: prathamshah94 <pratham.shah@cleanstart.com>

--- containers/busybox/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "🚀 Cleanstart BusyBox started as user: $(whoami)"
-echo "📦 BusyBox version: $(busybox | head -n 1)"
-
-# heartbeat logs every 5 sec
-while true; do
-  echo "⏳ Busybox is a combination of UNIX utilities and is a single binary! $(date)"
-  sleep 5
-done
